d666a40c14ca4d7d9b0e276db5ac64143bebfc40
João Nascimento <97626192+nascimento-jgb@users.noreply.github.com>
2023-05-24 10:08:32 +0300

    progress bar addapted

--- client/src/components/ProgressBar.tsx
+++ client/src/components/ProgressBar.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const ProgressBar = ({ progress }) => {
-	const getColor = (progress) => {
+interface ProgressBarProps {
+	progress: number;
+}
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ progress }) => {
+	const getColor = (progress: number): string => {
 		const colors = [
 			'rgb(174, 32, 18)',
 			'rgb(187, 62, 3)',
 			'rgb(251, 133, 0)',
-			'rgb(253, 158, 2'
+			'rgb(253, 158, 2)',
 		];
 
 		const index = Math.floor((progress - 1) / 25);
@@ -18,7 +22,7 @@ const ProgressBar = ({ progress }) => {
 	return (
 		<div className="outer-bar">
 			<div className="inner-bar" style={{ width: `${progress}%`, backgroundColor: color }}>
-			<span className="percentage">{`${progress}%`}</span>
+				<span className="percentage">{`${progress}%`}</span>
 			</div>
 		</div>
 	);
