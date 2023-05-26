5b8f4c5804a0162689eff495f6a247bf7af6cbdf
João Nascimento <97626192+nascimento-jgb@users.noreply.github.com>
2023-05-26 10:09:08 +0300

    blank ts file to client

--- client/src/components/TickIcon.tsx
+++ client/src/components/TickIcon.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const TickIcon: React.FC = () => {
 	return (
 		<svg
-			xmlns="http://www.w3.org/2000/svg"
+			xmlns="https://www.w3.org/2000/svg"
 			width="16"
 			height="16"
 			fill="currentColor"
